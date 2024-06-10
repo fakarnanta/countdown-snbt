e17740744d630a57788dbb906d41854b008e01a1
Daffa Damadhika Arkananta <92214010+fakarnanta@users.noreply.github.com>
2024-06-10 12:49:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const minutes = document.querySelector(".minutes .number");
 const hours = document.querySelector(".hours .number");
 const days = document.querySelector(".days .number");
 
-const targetDate = new Date("2024-05-20T15:00:00");
+const targetDate = new Date("2024-06-13T15:00:00");
 
 const timeFunction = setInterval(() => {
   const currentDate = new Date();
